f7e98099288a1c71dea5ef8cff8d8ad7f29f5397
konglt <konglt@518fbd03-09ad-f629-3e17-adcec8b035a5>
2012-08-23 07:05:23 +0000

    polished, works for FEFF9, but CFAVERAGE is disabled currently, as it seems not to work.

--- trunk/feff.cpp
+++ trunk/feff.cpp
@@ -82,13 +82,15 @@ void Driver::FEFF_main()
 
   // job type, CFAVERAGE or Clusters
   int flag_pbc = 0;
+  // disable this, since it does not work with FEFF
+/*
   printf("\nWould you like to run (1), an auto `average` over all absorbing atoms for\n");
   printf("each frame, or (2), individual calculations for each cluster? (1/2)[2]: ");
   if (count_words(fgets(str,MAXLINE,stdin)) > 0){
     char *ptr = strtok(str," \n\t\r\f");
     flag_pbc = atoi(ptr)%2;
   }
-  printf("Your selection : %d\n", 2-flag_pbc);
+  printf("Your selection : %d\n", 2-flag_pbc); */
 
   // XANES or EXAFS
   int job = 2;
@@ -132,9 +134,9 @@ void Driver::FEFF_main()
       fp = fopen(fname, "w");
 
       // write the potential part of feff.inp
-      fprintf(fp,"* Frame %d (MD steps: %d), %d types, %d atoms.\n",
+      fprintf(fp,"TITLE Frame %d (MD steps: %d), %d types, %d atoms.\n",
         img+1, one->tstep, one->ntype, one->natom);
-      fprintf(fp,"* Averaged over %d type-%d atoms.\n\n", one->nsel, AbsorberType);
+      fprintf(fp,"TITLE Averaged over %d type-%d atoms.\n\n", one->nsel, AbsorberType);
       fprintf(fp,"POTENTIALS\n*  ipot Z   tag lmax1 lmax2\n");
       char ename[3];
       for (int ip=1; ip <= one->ntype; ip++){
@@ -147,6 +149,8 @@ void Driver::FEFF_main()
       // reciprocal space calculation
       fprintf(fp,"\n* k-space calculation of crystals\nRECIPROCAL\n");
       fprintf(fp,"\n* Cartesian coordinates, Angstrom units\nCOORDINATES 1\n");
+      fprintf(fp,"\n* Core-hole by RPA\nCOREHOLE RPA\n");
+      fprintf(fp,"\n* KMesh needed\nKMESH 100 0\n");
       
       // lattice and atomic positions
       fprintf(fp,"\n* the lattice info\nLATTICE P 1.0\n");
@@ -209,9 +213,9 @@ void Driver::FEFF_main()
         fp = fopen(fname, "w");
   
         // write the potential part of feff.inp
-        fprintf(fp,"* Frame %d (MD steps: %d), %d types, %d atoms.\n",
+        fprintf(fp,"TITLE Frame %d (MD steps: %d), %d types, %d atoms.\n",
           img+1, one->tstep, one->ntype, one->natom);
-        fprintf(fp,"* Averaged over %d type %d atoms.\n\n", nc, AbsorberType);
+        fprintf(fp,"TITLE Averaged over %d type %d atoms.\n\n", nc, AbsorberType);
         fprintf(fp,"POTENTIALS\n*  ipot Z   tag lmax1 lmax2\n");
         char ename[3];
         for (int ip=1; ip <= one->ntype; ip++){
@@ -284,9 +288,9 @@ void Driver::FEFF_main()
           // write the potential part of feff.inp
           char ename[3];
           element->Num2Name(type2atnum[one->attyp[id]], ename);
-          fprintf(fp,"* Cluster centered on atom %d (%s) of frame %d (MD steps: %d)\n",
+          fprintf(fp,"TITLE Cluster centered on atom %d (%s) of frame %d (MD steps: %d)\n",
             id, ename, img+1, one->tstep);
-          fprintf(fp,"* Total number of atoms in cluster: %d\n\n", nclus);
+          fprintf(fp,"TITLE Total number of atoms in cluster: %d\n\n", nclus);
 
           fprintf(fp,"POTENTIALS\n*  ipot Z   tag lmax1 lmax2\n");
           fprintf(fp,"%4d   %3d  %3s  -1   3\n", 0, type2atnum[one->attyp[id]], ename);
@@ -297,18 +301,20 @@ void Driver::FEFF_main()
   
           // atomic positions
           one->dir2car();
-          fprintf(fp,"\n* Atomci positions in Angstrom\nATOMS\n* x y z ipot\n");
+          fprintf(fp,"\n* Atomci positions in Angstrom\nATOMS\n* x y z ipot tag dist id\n");
           for (std::list<int>::iterator it = cluster.begin(); it != cluster.end(); it++){
             int jd = *it;
             int jp = one->attyp[jd];
             if (jd == id) jp = 0;
-            double dx[3];
+            double dx[3], r2; r2 = 0.;
             for (int idim=0; idim<3; idim++){
               dx[idim] = one->atpos[jd][idim] - one->atpos[id][idim];
               while (dx[idim] > one->hbox[idim]) dx[idim] -= one->box[idim];
               while (dx[idim] <-one->hbox[idim]) dx[idim] += one->box[idim];
+              r2 += dx[idim]*dx[idim];
             }
-            fprintf(fp,"%15.8f %15.8f %15.8f %d\n", dx[0], dx[1], dx[2], jp);
+            element->Num2Name(type2atnum[one->attyp[jd]], ename);
+            fprintf(fp,"%15.8f %15.8f %15.8f %d %s %g %d\n", dx[0], dx[1], dx[2], jp, ename, sqrt(r2), jd);
           }
 
           // write other common info
@@ -334,46 +340,72 @@ return;
  *----------------------------------------------------------------------------*/
 void Driver::FEFF_input(int job, FILE *fp)
 {
-  fprintf(fp, "\n* Other structural information cards\n");
-  fprintf(fp, "* CIF cif_file * not needed here\n");
-  fprintf(fp, "\n* Tell the code that a real-space calculation is asked\nREAL\n");
-  fprintf(fp, "\n* If necessary, provided some comments/references as titles\n* TITLE any_thing\n");
-  fprintf(fp, "\n* scale factor of the distances\nRMULTIPLIER 1.00\n");
-  fprintf(fp, "\n\n* Spectrum Information cards\n");
+  double rfms1 = pow(90.*one->vol/(12.*one->natom), 1./3.);
+  double rfms2 = pow(600.*one->vol/(12.*one->natom), 1./3.);
+  double rfms3 = 2.2*pow(60.*one->vol/(12.*one->natom), 1./3.);
   if (job == 1){
-    fprintf(fp, "* XANES [xkmax xkstep vixan]\nXANES\n");
-    fprintf(fp, "* SCF rfms1\n* FMS rfms\n* RPATH rfms\n");
+    fprintf(fp, "* XANES [xkmax xkstep vixan]\nXANES 5.0\n");
+    fprintf(fp, "* SCF rfms1 * rfms1 must be converged for real space calculations\n");
+    fprintf(fp, "* SCF %g\n* FMS %g\n\n* RPATH %g\n", rfms1, rfms2, rfms3);
   } else {
-    fprintf(fp,"* EXAFS [xkmax]\nEXAFS\n");
-    fprintf(fp,"* RPATH rfms\n* SCF rfms1\n");
+    fprintf(fp,"* EXAFS [xkmax]\nEXAFS 20.0\n* RPATH %g\n* NLEG 8\n\n", rfms3);
+    fprintf(fp, "* SCF rfms1 * rfms1 must be converged for real space calculations\n");
+    fprintf(fp, "* SCF %g\n* FMS %g\n", rfms1, rfms2);
   }
-  fprintf(fp, "\n* If lDOS needed, uncomment and modify the following line\n");
-  fprintf(fp, "*    emin emax eimag\n* LDOS -20 20 0.2");
-  fprintf(fp, "\n\n* Control cards\n");
-  fprintf(fp, "*         ipot ixsph ifms ipaths igenfmt iff2x\n");
-  fprintf(fp, "  CONTROL  1    1     1    1      1       1\n");
-  fprintf(fp, "* PRINT \n");
-  fprintf(fp, "\n* DIMS nmax lmax\n* nmax: maximum # of atoms in the cluster for FMS matrix inversion\n");
-  fprintf(fp, "* lmax: maximal l-value for the potentials and Green`s function\n");
-  fprintf(fp, "\n* EGRID\n* This card can be used to customize the energy grid, which is followed by lines\n");
-  fprintf(fp, "* specifying the type, interval, incremental of the grid:\n");
-  fprintf(fp, "*    grid_type grid_min grid_max grid_step\n");
-  fprintf(fp, "* grid_type can be e_grid, k_grid, or exp_grid. In the latter case, Ei = Emin + exp(Estep * i) - 1\n");
-  fprintf(fp, "* It can be also user_grid, followed by energy points for the grid, one point in each line.\n");
-  fprintf(fp, "\n\n* Some other standard cards\n");
-  fprintf(fp, "* AFOLP folpx\n* This automatically overlaps all muffin-tins to a specified maximum value, default 1.15\n");
-  fprintf(fp, "* COREHOLE type\n* Determines how the core state is treated: none, equivalent to the NOHOLE card, meaning\n");
-  fprintf(fp, "*  there is no core hole; RPA, meaning the screen module calculates an RPA-screened core hole; FSR, a simple\n");
-  fprintf(fp, "*  Final state rule core hole (default). It is recommended to use RPA for k-space calculations.\n");
-  fprintf(fp, "* EDGE label s02\n* Sets the edge: label is `K`, `L1`...\nEDGE K 1.0\n");
-  fprintf(fp, "* SCF rfms1 [lfms1 nscmt ca nmix]\n* Controls FEFF`s automated SCF calculations.\n");
-  fprintf(fp, "* rfms1 specifies the radius of the cluster for full multiple scattering during SCF loop.\n");
-  fprintf(fp, "* S02 s02\n* Specifies the amplitude reduction factor S02.\n");
-  fprintf(fp, "\n* FMS rfms [lfms2 minv toler1 toler2 rdirec]\n");
-  fprintf(fp, "* Compute full multiple scattering within a sphere of radius rfms centered on the absorbing atom\n");
-  fprintf(fp, "* RPATH rpath\n* Determines the maximum effective (half-path) distance of a given path.\n");
-  fprintf(fp, "\n* DEBYE temperature Debye-temperature\n* Used to calculate the Debye-Waller factor.\n");
-  fprintf(fp, "\n* END\n");
+  fprintf(fp, "* COREHOLE type * type = none, RPA, or FSR (default)\n");
+
+  fprintf(fp, "\n* EDGE label s02\n* EDGE K 1.0\n");
+  fprintf(fp, "* REAL\n* RMULTIPLIER 1.00\n");
+  fprintf(fp, "\n*    emin emax eimag\n* LDOS -20 20 0.2\n");
+  fprintf(fp, "*       ipot ixsph ifms ipaths igenfmt iff2x\n");
+  fprintf(fp, "CONTROL  1    1     1    1      1       1\n");
+  fprintf(fp, "*         ppot pxsph pfms ppaths pgenfmt pff2x\n");
+  fprintf(fp, "* PRINT    0    0     0    0      0       0\n");
+
+  fprintf(fp, "\n\n* ***** Other useful or advanced cards *****\n");
+  fprintf(fp, "\n* *** pot related ***\n");
+  fprintf(fp, "* SO2 s02 * expr. value: 0.8-1.0\n");
+  fprintf(fp, "* EXCHANGE ixc vr0 vi0 [ixc0]\n");
+  fprintf(fp, "* AFOLP folpx  * folpx = 1.0 - 1.3; default 1.15\n");
+  fprintf(fp, "* FOLP ipot folpx\n* RGRID delta * default: 0.05\n");
+  fprintf(fp, "* SPIN ispin [x y z] * required for XMCD, SPXAS\n");
+  fprintf(fp, "* INTERSTITIAL inters vtot * change the way to get interstitial potential\n");
+  fprintf(fp, "* ION ipot ionization * ionizes atoms with unique potential index ipot\n");
+  fprintf(fp, "* UNFREEZEF * unfreeze f electrons\n");
+  fprintf(fp, "* CONFIG input [configurations] * modify the electron configuration\n");
+  fprintf(fp, "* CHSHIFT * calculate initial core state energy level using the final SCF potential\n");
+  fprintf(fp, "* CHBROADENING igammach\n* CHWIDTH gamma\n");
+  fprintf(fp, "* EGAP * works with MPSE only\n* EPS0 eps0 * works with MPSE only\n");
+  fprintf(fp, "* RESTART * restart SCF calculation from existing pot.bin\n");
+  fprintf(fp, "* SCREEN parameter value * pass custom parameters to screen\n");
+  fprintf(fp, "\n* *** xsph related ***\n");
+  fprintf(fp, "* ELLIPTICITY elpty x y z * used with POLARIZATION\n");
+  fprintf(fp, "* POLARIZATION  x y z\n* MULTIPOLE le2 [l2lp] * specify which multipole transition to include in the claculations\n");
+  fprintf(fp, "* LDEC ld * only for NRIXS\n* LJMAX lj * only for NRIXS\n");
+  fprintf(fp, "* MPSE ipl * runs FEFF wiht a many-pole model for the self energy\n");
+  fprintf(fp, "* RPHASES  * use real phase shifts rather than complex ones\n");
+  fprintf(fp, "* RSIGMA   * neglect the imaginary part of the self-energy\n");
+  fprintf(fp, "* TDLDA ixfc * use TDLDA to account for screening of the x-ray field and of the PE-core-hole interaction\n");
+  fprintf(fp, "\n* *** fms related ***\n");
+  fprintf(fp, "* DEBYE temperature Debye-temperature\n* Used to calculate the Debye-Waller factor.\n");
+  fprintf(fp, "* STRFAC eta gmax rmax\n");
+  fprintf(fp, "\n* *** path related ***\n");
+  fprintf(fp, "* NLEG nleg * limits the # of legs for each scattering path, default 8\n");
+  fprintf(fp, "* PCRITERIA pcritk pcrith * limit the # of paths\n");
+  fprintf(fp, "* SS index ipot deg rss   * works only with OVERLAP card\n");
+  fprintf(fp, "* SYMMETRY isym           * tells FEFF to reduce the list of paths using symmetry\n");
+  fprintf(fp, "\n* *** genfmt related ***\n");
+  fprintf(fp, "* CRITERIA critcw critpw  * limit the # of paths\n");
+  fprintf(fp, "* IORDER iord             * order of approximation used in module genfmt\n");
+  fprintf(fp, "* NSTAR                   * to write nstar.dat\n");
+  fprintf(fp, "\n* *** ff2x related ***\n");
+  fprintf(fp, "* CORRECTIONS vrcorr vicorr\n* ABSOLUTE\n* SIG2 sig2\n");
+  fprintf(fp, "\n* *** eels related ***\n");
+  fprintf(fp, "* MAGIC emagic\n");
+  
+  fprintf(fp, "\n* *** other ***\n");
+  fprintf(fp, "* DIMS nmax lmax\n\n* EGRID\n* grid_type grid_min grid_max grid_step\n");
+  fprintf(fp, "* KMESH nkp usesym symfile\n\n* END\n");
 
 return;
 }
@@ -419,7 +451,7 @@ void Driver::FEFF_voro(int flag, int vp, int &nmax, int **nlist, int *clist)
   double ylo = one->ylo, yhi = one->yhi, ly = one->ly;
   double zlo = one->zlo, zhi = one->zhi, lz = one->lz;
   double hx = 0.5*lx, hy = 0.5*ly, hz = 0.5*lz;
-  int n = one->natom;
+  int natom = one->natom;
 
   int *attyp = one->attyp;
   double **atpos = one->atpos;
@@ -428,12 +460,12 @@ void Driver::FEFF_voro(int flag, int vp, int &nmax, int **nlist, int *clist)
   one->dir2car();
 
   // compute optimal size for container, and then contrust it
-  double l = pow(double(n)/(5.6*lx*ly*lz), 1./3.);
+  double l = pow(double(natom)/(5.6*lx*ly*lz), 1./3.);
   int nx = int(lx*l+1), ny = int(ly*l+1), nz = int(lz*l+1);
   voro::container con(xlo,xhi,ylo,yhi,zlo,zhi,nx,ny,nz,true,true,true,8);
 
   // put atoms into the container
-  for (int i=1; i<= n; i++) con.put(i, one->atpos[i][0], one->atpos[i][1], one->atpos[i][2]);
+  for (int i=1; i<= natom; i++) con.put(i, one->atpos[i][0], one->atpos[i][1], one->atpos[i][2]);
 
   voro::voronoicell_neighbor c1, c2, *cell;
   voro::c_loop_all cl(con);
@@ -541,7 +573,7 @@ void Driver::FEFF_voro(int flag, int vp, int &nmax, int **nlist, int *clist)
     nf = fs.size();
     if (nf > nmax){
       while (nf >= nmax) nmax += 12;
-      nlist = memory->grow(nlist, nmax+1, one->natom+1, "nlist");
+      nlist = memory->grow(nlist, nmax+1, natom+1, "nlist");
     }
     nlist[0][id] = nf;
     for (int i=0; i<nf; i++) nlist[i+1][id] = neigh[i];
