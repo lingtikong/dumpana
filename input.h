dbe5bd97f1ac7354b2a846a647498583ac0480aa
LT Kong <konglt@sjtu.edu.cn>
2022-11-13 15:23:47 +0800

    Forgot input.h

---
+++ input.h
@@ -0,0 +1,23 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+class UserInput {
+public:
+   UserInput(int);
+   ~UserInput();
+
+   void read_stdin(char *);
+
+private:
+   int flag;
+   FILE *fp;
+
+};
+#endif
